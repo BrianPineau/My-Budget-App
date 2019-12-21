d8bdad1e299a98bfd1087b1da363d38e73cd7085
Brian Pineau <brianpineau@Brians-iMac.local>
2019-12-20 21:11:06 -0600

    Updated budget controller to not accept inputs that are out of the parameters

--- app.js
+++ app.js
@@ -28,7 +28,41 @@ var budgetController = (function () {
             exp : 0,
             inc : 0
         }
-    }
+    };
+
+    return {
+        addItem : function (type, des, val) {
+            let newItem, ID;
+
+            //[1 2 3 4 5], next ID = 6
+            //[1 2 4 6 8], next ID = 9
+            //ID = last ID + 1
+
+            //Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            //Create new item based on 'inc' or 'exp'
+            if (type === 'exp') {
+                newItem = new Expense (ID, des, val);
+            } else if (type == 'inc') {
+                newItem = new Income (ID, des, val); 
+            }
+
+            //Push it into our data structure
+            data.allItems[type].push(newItem);
+
+            //Return the new element
+            return newItem;
+        },
+
+        testing: function () {
+            console.log(data);
+        }
+    };
     
 })();
 
@@ -41,7 +75,9 @@ let UIController = (function () {
         inputType: '.add__type', 
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn'
+        inputBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
     }
 
     return {
@@ -49,8 +85,47 @@ let UIController = (function () {
             return {
                 type : document.querySelector(DOMstrings.inputType).value,   //Will be either inc or exp
                 description : document.querySelector(DOMstrings.inputDescription).value,
-                value : document.querySelector(DOMstrings.inputValue).value
+                value : parseFloat(document.querySelector(DOMstrings.inputValue).value)
             };
+        },
+
+        addListItem: function (obj, type) {
+            let html, newHtml, element;
+            //Create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+
+            }  else if (type === 'exp') {
+                element = DOMstrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+
+
+
+            //Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            //Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+
+        clearFields: function () {
+            let fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function (current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+
         },
 
         getDOMstrings: function () {
@@ -80,19 +155,36 @@ let controller = (function (budgetCtrl, UICtrl) {
     };
 
     
+    let updateBudget = function () {
+      
+        //  1.  Calculate the budget
+
+        //  2.  Return the budget
+
+        //  3.  Display the budget on the UI
+    };
 
     let ctrlAddItem = function () {
+        let input, newItem;
+
         //  1.  Get the filed input data
-        let input = UICtrl.getInput();
+        input = UICtrl.getInput();
 
+        if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
+            //  2.  Add the item to the budget controller
+            newItem = budgetController.addItem(input.type, input.description, input.value);
 
-        //  2.  Add the item to the budget controller
+            //  3.  Add the item to the UI
+            UICtrl.addListItem(newItem, input.type);
 
-        //  3.  Add the item to the UI
+            //  4.  Clear the fields
+            UICtrl.clearFields();
 
-        //  4.  Calculate the budget
+            //  5.  Calculate and update budget
+            updateBudget();
+        }
 
-        //  5.  Display the budget on the UI
+        
 
     };
 
